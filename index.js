31cbedad597b84b7f326231126bcc1ed8ef9a3c2
suyashpradhan <pradhansuyash39@gmail.com>
2021-04-15 23:25:33 +0530

    post request with express

--- index.js
+++ index.js
@@ -16,3 +16,38 @@ const productList = [
     price: 800,
   },
 ];
+
+//! Get Request
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+//! Get Request with data
+app.get("/products", (req, res) => {
+  const data = `
+  ${productList
+    .map((product) => {
+      return `<div>
+        <h1>${product.name}</h1>
+        <h3>${product.price}</h3>
+      </div>`;
+    })
+    .join("")}
+  `;
+  res.send(data);
+});
+
+//! Using Params
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const product = productList.find((product) => product.id === Number(id));
+  if (product) {
+    return res.json({ product, status: true });
+  }
+  res.json({ status: false, message: "Something went wrong!" });
+});
+
+//! ExpressJS Port
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
